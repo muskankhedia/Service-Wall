10402a737e135455ac07ae421ec0fc83d0155fdd
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-26 14:41:20 +0530

    process verification through net initiated

--- src/main/java/servicewall/App.java
+++ src/main/java/servicewall/App.java
@@ -1,7 +1,13 @@
 package servicewall;
 
+import servicewall.modules.*;
+
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Service-wall main process initiated!");
+        Processes processesObj = new Processes();
+        System.out.println(processesObj.getCurrentlyRunningProcesses());
     }
 }
