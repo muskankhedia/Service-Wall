45efd96260ff78af2d4128a78e05e217265b0e00
muskankhedia <muskan.khedia2000@gmail.com>
2019-06-05 11:34:54 +0530

    saves the data in different files

--- src/main/java/servicewall/data_gathering/Description_Process.java
+++ src/main/java/servicewall/data_gathering/Description_Process.java
@@ -13,73 +13,98 @@ import java.io.IOException;
 public class Description_Process {
 
 
-    private String searchProcess(String searchURL) throws IOException {
+    private String searchProcess(String searchURL)  {
 
-        Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
-        String text = doc.toString();
-        String subStr = "";
-        String str1 = "<h2>Description</h2>";
-        String str2 = "<h2>";
-        int lastIndex = text.indexOf(str1) + str1.length() + 1;
-        if (text.indexOf(str1) >= 1) {
-            subStr = text.substring(lastIndex);
-            int firstIndex = subStr.indexOf(str2);
-            if (firstIndex > 1){
-                subStr = subStr.substring(0,firstIndex);
+        try {
+            Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+            String text = doc.toString();
+            String subStr = "";
+            String str1 = "<h2>Description</h2>";
+            String str2 = "<h2>";
+            int lastIndex = text.indexOf(str1) + str1.length() + 1;
+            if (text.indexOf(str1) >= 1) {
+                subStr = text.substring(lastIndex);
+                int firstIndex = subStr.indexOf(str2);
+                if (firstIndex > 1){
+                    subStr = subStr.substring(0,firstIndex);
+                }
             }
+
+            String result =  Jsoup.parse(subStr).text();
+            return result;
         }
-
-        String result =  Jsoup.parse(subStr).text();
-        return result;
-
+        catch (Exception e) {
+            return null;
+        }
     }
 
     private void iterateAllLinks() throws IOException {
 
         JSONArray processList = new JSONArray();
-        for (int i = 1; i <= 1; i++) {
+        char initialLetter = '0';
+        for (int i = 1; i <= 10; i++) {
 
             String baseURL = "https://linux.die.net/man/";
 
             if (i < 9) {
                 baseURL = baseURL + i;
             } else if (i == 9) {
-                baseURL = baseURL +"l";
-            }else {
+                baseURL = baseURL + "l";
+            } else {
                 baseURL = baseURL + "n";
             }
 
-            System.out.println("basesURL: "+ baseURL);
+            System.out.println("basesURL: " + baseURL);
             Document doc = Jsoup.connect(baseURL).userAgent("Mozilla/5.0").get();
             Elements results = doc.select("dl > dt");
 
-            for (Element result: results) {
+            System.out.println("results:: " + results);
+            for (Element result : results) {
+
                 Elements link = result.getElementsByTag("a");
                 String processName = link.text();
-                if (!processName.equals("")){
-                    JSONObject processData = new JSONObject();
-                    JSONObject processObject = new JSONObject();
-                    String searchURL = baseURL + "/" + processName;
-                    System.out.println("searchURL:: "+ searchURL);
-                    String data = searchProcess(searchURL);
-                    System.out.println("processname: "+ processName);
-                    System.out.println("data: "+ data);
-                    processData.put("Process Name", processName);
-                    processData.put("Link", searchURL );
-                    processData.put("Description", data);
-                    processObject.put("Process", processData);
-                    processList.add(processObject);
+                if (!processName.equals("")) {
+                    if (processName.charAt(0) == initialLetter) {
+                        JSONObject processData = new JSONObject();
+                        JSONObject processObject = new JSONObject();
+                        String searchURL = baseURL + "/" + processName;
+                        System.out.println("searchURL:: " + searchURL);
+                        String data = searchProcess(searchURL);
+                        System.out.println("processname: " + processName);
+                        System.out.println("data: " + data);
+                        processData.put("Process Name", processName);
+                        processData.put("Link", searchURL);
+                        processData.put("Description", data);
+                        processObject.put("Process", processData);
+                        processList.add(processObject);
+                    } else {
+                        //Write JSON file
+                        String fileName  = "processdesc" + i + initialLetter + ".json";
+                        try (FileWriter file = new FileWriter(fileName)) {
+                            initialLetter += 1 ;
+                            file.write(processList.toJSONString());
+                            file.flush();
+                            processList = new JSONArray();
+                            if (processName.charAt(0) == initialLetter){
+                                JSONObject processData = new JSONObject();
+                                JSONObject processObject = new JSONObject();
+                                String searchURL = baseURL + "/" + processName;
+                                System.out.println("searchURL:: " + searchURL);
+                                String data = searchProcess(searchURL);
+                                System.out.println("processname: " + processName);
+                                System.out.println("data: " + data);
+                                processData.put("Process Name", processName);
+                                processData.put("Link", searchURL);
+                                processData.put("Description", data);
+                                processObject.put("Process", processData);
+                                processList.add(processObject);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
-        }
-        //Write JSON file
-        try (FileWriter file = new FileWriter("processdesc.json")) {
-
-            file.write(processList.toJSONString());
-            file.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -87,7 +112,7 @@ public class Description_Process {
 
     public static void main(String[] args) throws IOException {
         Description_Process p = new Description_Process();
-//        System.out.println(p.searchProcess("https://linux.die.net/man/1/ace.pl"));
+//        System.out.println(p.searchProcess("https://linux.die.net/man/1/cheatmake"));
         p.iterateAllLinks();
     }
 
